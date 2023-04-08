c6cca650834b386f3cf1dc9fcb1d64cbecb71681
serkanboyraci <serkanboyraci@gmail.com>
2023-04-08 12:20:13 +0200

    JokesData added

---
+++ JokesAppSwiftUI/JokesData.swift
@@ -0,0 +1,21 @@
+//
+//  JokesData.swift
+//  JokesAppSwiftUI
+//
+//  Created by Ali serkan Boyracı  on 8.04.2023.
+//
+
+import Foundation
+
+// MARK: - Crypto
+struct Welcome: Codable {
+    var type: String
+    var value: [Value]
+}
+
+// MARK: - Value
+struct Value: Codable {
+    var id: Int
+    var joke: String
+    var categories: [String]
+}
